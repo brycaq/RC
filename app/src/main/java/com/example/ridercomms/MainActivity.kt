13d85b4be885f02465c68a5915e651b0cc8ead9f
brycaq <brycaqs@gmail.com>
2026-08-19 07:29:38 +0800

    Update MainActivity.kt

--- app/src/main/java/com/example/ridercomms/MainActivity.kt
+++ app/src/main/java/com/example/ridercomms/MainActivity.kt
@@ -9,14 +9,13 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.ridercomms.ui.screens.MainScreen
-import com.example.ridercomms.ui.theme.RiderCommsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            RiderCommsTheme {
+            MaterialTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
